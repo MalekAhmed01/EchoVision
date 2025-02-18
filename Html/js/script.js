10bf637ea06fd0aa967e70f37a5868c0de23d3a2
MalekAhmed01 <malekahmed4514@gmail.com>
2025-02-18 04:21:35 +0200

    Fix login and signup screens and modify home screen

---
+++ Html/js/script.js
@@ -0,0 +1,14 @@
+
+
+ document.getElementById("cal").onclick = function()
+{
+    var x =document.getElementById("x").value;
+    var y =document.getElementById("y").value;
+
+    var total=x*y;
+    document.getElementById("res").innerHTML="Your res is : "+total;
+
+
+
+
+} 
